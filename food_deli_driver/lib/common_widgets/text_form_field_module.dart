42a7f2daa0560057843d67e3461e3a50435cebae
Reshma <reshma@omtecweb.com>
2023-02-09 11:05:53 +0530

    Update text_form_field_module.dart

--- food_deli_driver/lib/common_widgets/text_form_field_module.dart
+++ food_deli_driver/lib/common_widgets/text_form_field_module.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:food_deli_driver/utils/colors.dart';
+import 'package:food_deli_driver/utils/extensions.dart';
 import 'package:food_deli_driver/utils/styles.dart';
+import 'package:sizer/sizer.dart';
 
 class CommonTextFormFieldModule extends StatelessWidget {
   TextEditingController controller;
-  String labelText;
+  // String labelText;
+  final String headerText;
+  final String mandatoryText;
   String hintText;
   bool obscureText;
   final FormFieldValidator? validate;
@@ -15,11 +20,12 @@ class CommonTextFormFieldModule extends StatelessWidget {
   int? maxLength;
   List<TextInputFormatter>? inputFormatters;
 
-
   CommonTextFormFieldModule({
     Key? key,
     required this.controller,
-    required this.labelText,
+    required this.headerText,
+    required this.mandatoryText,
+    // required this.labelText,
     required this.hintText,
     this.obscureText = false,
     this.validate,
@@ -33,27 +39,53 @@ class CommonTextFormFieldModule extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return TextFormField(
-      controller: controller,
-      obscureText: obscureText,
-      validator: validate,
-      keyboardType: keyboardType,
-      readOnly: readOnly,
-      maxLength: maxLength,
-      inputFormatters: inputFormatters,
-      cursorColor: Colors.grey.shade700,
-      autovalidateMode: AutovalidateMode.onUserInteraction,
-      decoration: InputDecoration(
-        enabledBorder: InputFieldStyles().inputBorder(),
-        focusedBorder: InputFieldStyles().inputBorder(),
-        errorBorder: InputFieldStyles().inputBorder(),
-        focusedErrorBorder: InputFieldStyles().inputBorder(),
-        counterText: '',
-        labelText: labelText,
-        hintText: hintText,
-        labelStyle: TextStyle(color: Colors.grey.shade700),
-        suffixIcon: suffixIcon,
-      ),
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        RichText(
+          textAlign: TextAlign.left,
+          maxLines: null,
+          text: TextSpan(
+              text: headerText,
+              style: TextStyleConfig.textStyle(
+                fontWeight: FontWeight.w600,
+                fontSize: 16,
+              ),
+              children: [
+                TextSpan(
+                  text: ' $mandatoryText',
+                  style: TextStyleConfig.textStyle(
+                    textColor: AppColors.redColor,
+                    fontWeight: FontWeight.w600,
+                    fontSize: 16,
+                  ),
+                ),
+              ]),
+        ).commonSymmetricPadding(vertical: 4),
+        SizedBox(height: 1.h),
+        TextFormField(
+          controller: controller,
+          obscureText: obscureText,
+          validator: validate,
+          keyboardType: keyboardType,
+          readOnly: readOnly,
+          maxLength: maxLength,
+          inputFormatters: inputFormatters,
+          cursorColor: Colors.grey.shade700,
+          autovalidateMode: AutovalidateMode.onUserInteraction,
+          decoration: InputDecoration(
+            enabledBorder: InputFieldStyles().inputBorder(),
+            focusedBorder: InputFieldStyles().inputBorder(),
+            errorBorder: InputFieldStyles().inputBorder(),
+            focusedErrorBorder: InputFieldStyles().inputBorder(),
+            counterText: '',
+            // labelText: labelText,
+            hintText: hintText,
+            labelStyle: TextStyle(color: Colors.grey.shade700),
+            suffixIcon: suffixIcon,
+          ),
+        ),
+      ],
     );
   }
 }
